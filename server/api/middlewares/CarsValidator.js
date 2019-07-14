1359ef0966417b74b0fd224ea844e96b3bfafc1a
Anifowose Habeeb <anifowosehabeeb@gmail.com>
2019-07-14 09:08:35 +0100

    fix commit

--- server/api/middlewares/CarsValidator.js
+++ server/api/middlewares/CarsValidator.js
@@ -27,16 +27,13 @@ class CarsValidator {
    * @param {function} next - Passes control to next middleware
    */
   static createAd(req, res, next) {
-    upload(req, res, async (err) => {
-      if (err || err instanceof multer.MulterError) {
-        // A Multer error occurred when uploading.
-        ResponseHandler.error(res, 415, 'Images uploaded are not valid');
-        return;
+    if (req.body.img_url) {
+      const imgUrl = req.body.img_url;
+      if (typeof imgUrl === 'object') {
+        req.body.imageUrlList = imgUrl;
+      } else {
+        req.body.imageUrlList = imgUrl.split(' ');
       }
-
-      // Everything went fine.
-      console.log(req.files);
-      const imgFiles = req.files;
 
       const bodyType = req.body.body_type;
       const {
@@ -67,8 +64,15 @@ class CarsValidator {
         return;
       }
 
-      if (manufacturer === undefined || String(manufacturer).trim() === '') {
-        ResponseHandler.error(res, 400, 'manufacturer is undefined or invalid');
+      if (
+        manufacturer === undefined
+        || String(manufacturer).trim() === ''
+      ) {
+        ResponseHandler.error(
+          res,
+          400,
+          'manufacturer is undefined or invalid',
+        );
         return;
       }
 
@@ -78,42 +82,99 @@ class CarsValidator {
       }
 
       if (bodyType === undefined || bodyType.trim() === '') {
-        ResponseHandler.error(res, 400, 'body type is undefined or invalid');
-        return;
+        ResponseHandler.error(
+          res,
+          400,
+          'body type is undefined or invalid',
+        );
       }
+    } else {
+      upload(req, res, async (err) => {
+        if (err || err instanceof multer.MulterError) {
+          // A Multer error occurred when uploading.
+          ResponseHandler.error(res, 415, 'Images uploaded are not valid');
+          return;
+        }
 
-      // Restrict images to 1 - 6
-      if (imgFiles.length <= 0 || imgFiles.length > 6) {
-        ResponseHandler.error(res, 400, 'please add between 1 and 6 images');
-        return;
-      }
-      const imageList = [];
+        // Everything went fine.
+        const imgFiles = req.files;
 
-      for (let i = 0; i < imgFiles.length; i += 1) {
-        const image = await imageUploader(req.files[i]);
-        // delete files after upload
-        await unlinkAsync(imgFiles[i].path);
-        if (image === -1) {
+        const bodyType = req.body.body_type;
+        const {
+          state, price, manufacturer, model,
+        } = req.body;
+
+        if (state === undefined || String(state).trim() === '') {
+          ResponseHandler.error(res, 400, 'vehicle state is undefined');
+          return;
+        }
+
+        if (state !== 'new' && state !== 'used') {
           ResponseHandler.error(
             res,
-            502,
-            'error uploading the images to cloudinary',
+            400,
+            'vehicle state can only be new or used',
           );
           return;
         }
-        imageList.push(image);
-      }
 
-      req.body.imageUrlList = imageList;
+        if (price === undefined || String(price).trim() === '') {
+          ResponseHandler.error(res, 400, 'price is undefined');
+          return;
+        }
 
-      next();
-    });
+        if (!validator.isNumeric(String(price))) {
+          ResponseHandler.error(res, 400, 'price is not a number');
+          return;
+        }
+
+        if (manufacturer === undefined || String(manufacturer).trim() === '') {
+          ResponseHandler.error(res, 400, 'manufacturer is undefined or invalid');
+          return;
+        }
+
+        if (model === undefined || String(model).trim() === '') {
+          ResponseHandler.error(res, 400, 'model undefined or invalid');
+          return;
+        }
+
+        if (bodyType === undefined || bodyType.trim() === '') {
+          ResponseHandler.error(res, 400, 'body type is undefined or invalid');
+          return;
+        }
+
+        // Restrict images to 1 - 6
+        if (imgFiles.length <= 0 || imgFiles.length > 6) {
+          ResponseHandler.error(res, 400, 'please add between 1 and 6 images');
+          return;
+        }
+        const imageList = [];
+
+        for (let i = 0; i < imgFiles.length; i += 1) {
+          const image = await imageUploader(req.files[i]);
+          // delete files after upload
+          await unlinkAsync(imgFiles[i].path);
+          if (image === -1) {
+            ResponseHandler.error(
+              res,
+              502,
+              'error uploading the images to cloudinary',
+            );
+            return;
+          }
+          imageList.push(image);
+        }
+
+        req.body.imageUrlList = imageList;
+
+        next();
+      });
+    }
   }
 
   static verifyImage(req, res, next) {
     console.log(typeof req.body.image_url);
     console.log(req.body.image_url);
-    console.log(`IMG_URL ${req.body.img_url}`);
     next();
   }
 
